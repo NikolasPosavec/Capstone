0def0f8058978089cd66ada5e1d2d2544669c9a0
NikolasPosavec <nikolas.posavec@gmail.com>
2025-02-22 22:50:50 -0500

    maed

---
+++ capstone_website/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+    return (
+      <div className="page-container">
+        <h1 className="title">Welcome to The Evolution of Programming Languages</h1>
+        <p className="description">Explore how programming languages have evolved over time.</p>
+        <button className="interactive-button">Learn More</button>
+      </div>
+    );
+  }
---
+++ capstone_website/src/pages/TestBench.jsx
@@ -0,0 +1,9 @@
+export function TestBench() {
+    return (
+      <div className="page-container">
+        <h1 className="title">Programming Language Test Bench</h1>
+        <p className="description">Compare different programming languages based on performance and syntax.</p>
+        <button className="interactive-button">Run Tests</button>
+      </div>
+    );
+  }
